a2a27d3251e88c37b3ab0a5279adacf0a0a450a3
77ircode <77icode@gmail.com>
2025-07-18 16:16:45 +0800

    nginx-webdav

---
+++ nginx-webdav/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+nginx_conf_path=/etc/nginx/nginx.conf
+
+if [[ -n "$USERNAME" ]] && [[ -n "$PASSWORD" ]]
+then
+	htpasswd -bc /etc/nginx/htpasswd $USERNAME $PASSWORD
+	echo Done.
+else
+    echo Using no auth.
+	sed -i 's%auth_basic "Restricted";% %g' nginx_conf_path
+	sed -i 's%auth_basic_user_file htpasswd;% %g' nginx_conf_path
+fi
+
+if [[ -n "$CLIENT_BODY_TEMP_PATH" ]]
+then
+	sed -i "s%client_body_temp_path;% client_body_temp_path $CLIENT_BODY_TEMP_PATH;%g" nginx_conf_path
+	echo Done.
+else
+    echo Using no auth.
+	sed -i 's%client_body_temp_path;% %g' nginx_conf_path
+fi
